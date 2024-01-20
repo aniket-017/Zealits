252ddc1bb7375aff32d8eb7c6824d96cee728218
Aniket Arjun Khillare <aniketkhillare17@gmail.com>
2024-01-20 16:14:30 +0530

    working on Service section

--- frontend/src/Pages/Services.js
+++ frontend/src/Pages/Services.js
@@ -1,40 +1,142 @@
-import React from 'react'
-import "./Services.css"
+import React from 'react';
+import './Services.css';
+import website from "../Assets/Photos/Services/data.png"
+
 
 const Services = () => {
   return (
     <div>
-        <section id="services">
-    <div class="services-content">
-        <h2>Our Services</h2>
-        <p class="services-description">
+      <section id="services">
+        <div className="services-content">
+          <h2>Our Services</h2>
+          <p className="services-description">
             Our seasoned technical and project management team will guide you from a business idea to the implementation of a cost-effective software solution. We deliver valuable business tools specially designed to serve the specific needs of your business.
-        </p>
-        <div class="services-list">
-            <div class="service1">
-                <h3>Web/Mobile</h3>
-                <ul>
-                    <li>Enterprise Portal Development</li>
-                    <li>Website Design, Re-design, Branding, and Migration</li>
-                    <li>Mobile Applications - Android and iPhone Apps</li>
-                    <li>Web and Mobile Analytics</li>
-                </ul>
+          </p>
+          <div className="card-section">
+            {/* Card 1 */}
+            <div className="card">
+              <div className="content">
+              <img />
+                <h2 className="title">Mountain View</h2>
+                {/* <p className="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p> */}
+                {/* <button className="btn">View Trips</button> */}
+              </div>
             </div>
-            <div class="service1">
-                <h3>Information Technology</h3>
-                <ul>
-                    <li>SEO Consulting</li>
-                    <li>Cloud Infrastructure Solutions</li>
-                    <li>Application Development & Migration</li>
-                    <li>Six Sigma Business Solutions</li>
-                    <li>Target Marketing</li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</section>
-    </div>
-  )
-}
 
-export default Services
+            {/* Card 2 */}
+            {/* <div className="card">
+              <div className="content" >
+               
+                <h2 className="title">To The Beach</h2>
+                <p className="copy">Plan your next beach trip with these fabulous destinations</p>
+                <button className="btn">View Trips</button>
+              </div>
+            </div> */}
+          </div>
+        </div>
+      </section>
+
+      <section className="section-2">
+      <div className="heading">
+        <h1>Image Card Hover Effect #02</h1>
+      </div>
+      <figure className="figure">
+        <img src="https://images.pexels.com/photos/1510610/pexels-photo-1510610.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Tokyo" />
+        <figcaption>
+          <h3 className="title">Tokyo</h3>
+          <h3 className="hover">Read More</h3>
+        </figcaption>
+        <a href="#"></a>
+      </figure>
+      <figure className="figure">
+        <img src="https://images.pexels.com/photos/1738994/pexels-photo-1738994.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="Hong Kong" />
+        <figcaption>
+          <h3 className="title">Hong Kong</h3>
+          <h3 className="hover">Read More</h3>
+        </figcaption>
+        <a href="#"></a>
+      </figure>
+      <figure className="figure">
+        <img src="https://images.pexels.com/photos/3031750/pexels-photo-3031750.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Seoul" />
+        <figcaption>
+          <h3 className="title">Seoul</h3>
+          <h3 className="hover">Read More</h3>
+        </figcaption>
+        <a href="#"></a>
+      </figure>
+    </section>
+
+
+    <section className="section-4">
+      <div className="heading">
+        <h1>Image Card Hover Effect #04</h1>
+      </div>
+      <div className="row">
+        {/* <figure className="figure">
+          <img src="https://images.pexels.com/photos/1955134/pexels-photo-1955134.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" />
+          <div>
+            <h3>Empty <span>Highway</span></h3>
+            <a href="#"></a>
+          </div>
+        </figure>
+        <figure className="figure">
+          <img src="https://images.pexels.com/photos/1707820/pexels-photo-1707820.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" />
+          <div>
+            <h3>City <span>Roadway</span></h3>
+            <a href="#"></a>
+          </div>
+        </figure> */}
+        <figure className="figure">
+          
+          <div className='heading'>
+            <h3>Website</h3>
+          </div>
+          <div className='details'>
+            <h3>Website</h3>
+            <ul>
+                <li>Eccomerce</li>
+                <li>Website</li>
+                <li>Formus</li>
+                <li>Blog</li>
+            </ul>
+          </div>
+        </figure>
+
+        <figure className="figure">
+          
+          <div className='heading'>
+            <h3>Website</h3>
+          </div>
+          <div className='details'>
+            <h3>Website</h3>
+            <ul>
+                <li>Eccomerce</li>
+                <li>Website</li>
+                <li>Formus</li>
+                <li>Blog</li>
+            </ul>
+          </div>
+        </figure>
+
+        <figure className="figure">
+          <img className='picture' src={website} />
+          <div className='heading'>
+            <h3 className='head'>Website</h3>
+          </div>
+          <div className='details'>
+            <h3>Website</h3>
+
+            <p>Eccomerce</p>
+            <p>Eccomerce</p>
+            <p>Eccomerce</p>
+            <p>Eccomerce</p>
+            
+          </div>
+        </figure>
+      </div>
+    </section>
+    </div>
+  );
+};
+
+export default Services;
